db1e5c471479d00b4315857001faeb22ce1c2e31
YERIEL-RYU <yeriel.ryu@gmail.com>
2021-10-19 10:22:55 +0900

    2-1 Apollo server 구축하기

---
+++ index.js
@@ -0,0 +1,29 @@
+const database = require('./database')
+const { ApolloServer, gql } = require('apollo-server')
+const typeDefs = gql`
+  type Query {
+    teams: [Team]
+  }
+  type Team {
+    id: Int
+    manager: String
+    office: String
+    extension_number: String
+    mascot: String
+    cleaning_duty: String
+    project: String
+  }
+`
+const resolvers = {
+  Query: {
+    teams: () => database.teams
+  }
+}
+
+//server 생성 ApolloServer
+const server = new ApolloServer({ typeDefs, resolvers })
+
+//server 실행 listen()
+server.listen().then(({ url }) => {
+console.log(`🚀  Server ready at ${url}`)
+})
